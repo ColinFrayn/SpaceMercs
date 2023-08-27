bd8081b500e0f180742f5b9d0d8e471f45ff2e57
Colin Frayn <119124689+ColinFrayn@users.noreply.github.com>
2023-08-27 19:50:12 +0100

    Slightly tweaking power requirements of ship equipment; More tests;

--- UnitTests/TestSoldier.cs
+++ UnitTests/TestSoldier.cs
@@ -18,7 +18,6 @@ namespace UnitTests {
             ItemType? typ = StaticData.GetItemTypeByName("Medikit");
             Assert.NotNull(typ);
             Equipment eq = new Equipment(typ);
-            Assert.NotNull(eq);
             soldier.AddItem(eq);
             ReadOnlyDictionary<IItem, int> inventoryGrouped = soldier.InventoryGrouped;
             Assert.AreEqual(1, inventoryGrouped.Count);
@@ -29,11 +28,9 @@ namespace UnitTests {
         [Test]
         public void Test_AddMultipleItems() {
             Soldier soldier = new Soldier("Bob", humanRace, 10, 10, 10, 10, 10, GenderType.Male, 1, 0);
-            Assert.AreEqual(0, soldier.InventoryGrouped.Count);
             ItemType? typ = StaticData.GetItemTypeByName("Medikit");
             Assert.NotNull(typ);
             Equipment eq = new Equipment(typ);
-            Assert.NotNull(eq);
             soldier.AddItem(eq,20);
             ReadOnlyDictionary<IItem, int> inventoryGrouped = soldier.InventoryGrouped;
             Assert.AreEqual(1, inventoryGrouped.Count);
@@ -44,15 +41,12 @@ namespace UnitTests {
         [Test]
         public void Test_AddMultipleItems_ShouldStackSeparately() {
             Soldier soldier = new Soldier("Bob", humanRace, 10, 10, 10, 10, 10, GenderType.Male, 1, 0);
-            Assert.AreEqual(0, soldier.InventoryGrouped.Count);
             ItemType? typ1 = StaticData.GetItemTypeByName("Medikit");
             Assert.NotNull(typ1);
             Equipment eq1 = new Equipment(typ1);
-            Assert.NotNull(eq1);
             ItemType? typ2 = StaticData.GetItemTypeByName("Frag Grenade");
             Assert.NotNull(typ2);
             Equipment eq2 = new Equipment(typ2);
-            Assert.NotNull(eq2);
             soldier.AddItem(eq1);
             soldier.AddItem(eq2);
             soldier.AddItem(eq1);
@@ -64,6 +58,47 @@ namespace UnitTests {
             Assert.Contains(eq2, inventoryGrouped.Keys);
             Assert.AreEqual(3, inventoryGrouped[eq1]);
             Assert.AreEqual(2, inventoryGrouped[eq2]);
+        }
+
+        [Test]
+        public void Test_RemoveItemByType() {
+            Soldier soldier = new Soldier("Bob", humanRace, 10, 10, 10, 10, 10, GenderType.Male, 1, 0);
+            ItemType? typ = StaticData.GetItemTypeByName("Medikit");
+            Assert.NotNull(typ);
+            Equipment eq = new Equipment(typ);
+            soldier.AddItem(eq);
+            soldier.RemoveItemByType(typ);
+            ReadOnlyDictionary<IItem, int> inventoryGrouped = soldier.InventoryGrouped;
+            Assert.AreEqual(0, inventoryGrouped.Count);
+        }
+
+        [Test]
+        public void Test_RemoveItemByType_WrongType_ShouldBeNOP() {
+            Soldier soldier = new Soldier("Bob", humanRace, 10, 10, 10, 10, 10, GenderType.Male, 1, 0);
+            ItemType? typ = StaticData.GetItemTypeByName("Medikit");
+            Assert.NotNull(typ);
+            ItemType? typ2 = StaticData.GetItemTypeByName("Frag Grenade");
+            Assert.NotNull(typ2);
+            Equipment eq = new Equipment(typ);
+            soldier.AddItem(eq);
+            soldier.RemoveItemByType(typ2);
+            ReadOnlyDictionary<IItem, int> inventoryGrouped = soldier.InventoryGrouped;
+            Assert.AreEqual(1, inventoryGrouped.Count);
+            Assert.Contains(eq, inventoryGrouped.Keys);
+            Assert.AreEqual(1, inventoryGrouped[eq]);
+        }
+
+        [Test]
+        public void Test_HasItem() {
+            Soldier soldier = new Soldier("Bob", humanRace, 10, 10, 10, 10, 10, GenderType.Male, 1, 0);
+            ItemType? typ = StaticData.GetItemTypeByName("Medikit");
+            Equipment eq = new Equipment(typ);
+            ItemType? typ2 = StaticData.GetItemTypeByName("Frag Grenade");
+            Assert.NotNull(typ2);
+            Equipment eq2 = new Equipment(typ2);
+            soldier.AddItem(eq);
+            Assert.True(soldier.HasItem(eq));
+            Assert.False(soldier.HasItem(eq2));
         }
     }
 }
